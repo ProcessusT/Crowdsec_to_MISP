f3835a19a567db19a72a3966b538a24cfce1d2d0
Processus Thief <processus.thief@gmail.com>
2023-03-10 11:05:57 +0100

    Update crowdsec_to_misp.py

--- crowdsec_to_misp.py
+++ crowdsec_to_misp.py
@@ -5,7 +5,7 @@ import time
 import os, sys
 import sqlite3
 import re
-from pymisp import PyMISP
+from pymisp import PyMISP, MISPEvent
 
 
 try:
@@ -31,18 +31,36 @@ try:
                         print(err)
                         sys.exit(1)
 
-                # CROWDSEC-IPS event id
-                event_id=<YOUR MISP EVENT ID HERE>
+                
 
                 # MISP config keys
-                misp_url='https://<YOUR MISP DNS NAME HERE>:8085/'
-                misp_key='<YOU MISP API KEY HERE>'
+                misp_url='https://<MISP URL>'
+                misp_key='<MISP API KEY>'
                 misp_verifycert=True
 
                 try:
+
                         pymisp=PyMISP(misp_url, misp_key, misp_verifycert)
+
+                        oldevent = pymisp.search(eventinfo='CROWDSEC DB', metadata=True)
+                        oldevent_id = oldevent[0]['Event']['id']
+
+                        
+                        event = MISPEvent()
+                        event.distribution = 1
+                        event.threat_level_id = 1
+                        event.analysis = 0
+                        event.info = "CROWDSEC DB"
+                        event = pymisp.add_event(event, pythonify=True)
+
+                        # CROWDSEC-IPS event id
+                        event_id=event.id
+
                         for ip in crowdsec_ips:
                                 result=pymisp.add_attribute(event_id, {'type': 'ip-dst', 'value': str(ip), 'comment': 'From Crowdsec community decisions'}, pythonify=True)
+
+                        # on delete l'ancien event
+                        misp.delete_event(oldevent_id)
                 except Exception as err:
                         print(err)
                         sys.exit(1)
